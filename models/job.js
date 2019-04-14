0b58fb21bb4ef1aa4db5b408d01ba5a4aad23ea5
Julu Ahamed <julu@marax.in>
2019-04-14 23:29:23 +0530

    Create Job endpoint

---
+++ models/job.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+mongoose.Promise = global.Promise;
+
+module.exports =mongoose.model('Job', new Schema({
+  name: {type: String, index: true},
+  type: {type: String},
+  sub_type: {type: String},
+  stages:[{name: String, owner: String, status: String, comment: [String], callcount: {type: Number, default: null}}],
+  history:[{name: String, owner: String, status: String}],
+  profile: {type: [{name: {type: String, index: true}, pan: {type:String, index: true}}], default: null},
+  currentStage: {
+    stage: Number, 
+    data: {
+      name: String, 
+      owner: {type:String, index:true},
+      status: {type:String, index:true}
+    }
+  },
+  documents:[{
+    type: String,
+    url: String,
+    date: {type:Schema.Types.Mixed}
+  }],
+  payment: {type:Schema.Types.Mixed, index: true},
+}, { collection: 'Job',
+      timestamps: {
+        createdAt: 'createdAt',
+        updatedAt: 'updatedAt',
+      },
+    }
+  ));
